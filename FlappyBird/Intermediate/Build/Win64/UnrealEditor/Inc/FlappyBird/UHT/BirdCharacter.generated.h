7973d114e813778c1a86e122592afde7d9b25a94
NMThuan0010 <nguyenminhthuan0010@gmail.com>
2025-08-03 10:42:59 +0700

    first_code_Bird_Character

---
+++ FlappyBird/Intermediate/Build/Win64/UnrealEditor/Inc/FlappyBird/UHT/BirdCharacter.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "BirdCharacter.h"
+
+#ifdef FLAPPYBIRD_BirdCharacter_generated_h
+#error "BirdCharacter.generated.h already included, missing '#pragma once' in BirdCharacter.h"
+#endif
+#define FLAPPYBIRD_BirdCharacter_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class ABirdCharacter ***********************************************************
+FLAPPYBIRD_API UClass* Z_Construct_UClass_ABirdCharacter_NoRegister();
+
+#define FID_FlappyBird_Source_FlappyBird_BirdCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABirdCharacter(); \
+	friend struct Z_Construct_UClass_ABirdCharacter_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend FLAPPYBIRD_API UClass* Z_Construct_UClass_ABirdCharacter_NoRegister(); \
+public: \
+	DECLARE_CLASS2(ABirdCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/FlappyBird"), Z_Construct_UClass_ABirdCharacter_NoRegister) \
+	DECLARE_SERIALIZER(ABirdCharacter)
+
+
+#define FID_FlappyBird_Source_FlappyBird_BirdCharacter_h_12_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	ABirdCharacter(ABirdCharacter&&) = delete; \
+	ABirdCharacter(const ABirdCharacter&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABirdCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABirdCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ABirdCharacter) \
+	NO_API virtual ~ABirdCharacter();
+
+
+#define FID_FlappyBird_Source_FlappyBird_BirdCharacter_h_9_PROLOG
+#define FID_FlappyBird_Source_FlappyBird_BirdCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_FlappyBird_Source_FlappyBird_BirdCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	FID_FlappyBird_Source_FlappyBird_BirdCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class ABirdCharacter;
+
+// ********** End Class ABirdCharacter *************************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_FlappyBird_Source_FlappyBird_BirdCharacter_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
